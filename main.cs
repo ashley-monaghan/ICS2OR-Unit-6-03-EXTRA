f80b2493801e8eb5da92406d0ed53626c60f2e91
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2023-01-12 14:07:21 +0000

    sync

--- main.cs
+++ main.cs
@@ -5,42 +5,30 @@
 
 using System;
 using System.Threading.Tasks;
-using System.IO;
-
+using System.Net.Http;
+using System.Text.Json.Nodes;
+ 
 class Program
 {
     public static async Task Main()
     {
 
-        string userAnswer;
-        int counter = 0;
-
-        Console.WriteLine("Hello user! Would you like to quit?");
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://api.openweathermap.org/data/2.5/weather lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+        );
+        // Console.WriteLine(response);
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        // Console.WriteLine(jsonAsDictionary);
         Console.WriteLine("");
-
-        Console.WriteLine("Enter either yes or no ");
-        Console.WriteLine("");
-        userAnswer = (Console.ReadLine());
-
-        while (userAnswer == "no")
-        {
-            counter++;
-            string text = "Total: " + counter;
-
-            await File.WriteAllTextAsync("WriteText.txt", text);
-            Console.WriteLine("File Updated...");
-            Console.WriteLine("");
-
-            var someText = await File.ReadAllTextAsync(@"WriteText.txt");
-            Console.WriteLine(someText);
-            Console.WriteLine("");
-
-            Console.WriteLine("How about now? Enter either yes or no: ");
-            userAnswer = (Console.ReadLine());
-            Console.WriteLine("");
-        };
-
-        Console.WriteLine("Good bye!");
+        JsonNode forecastNode = JsonNode.Parse(response)!;
+        // Console.WriteLine(forecastNode);
+        JsonNode mainNode = forecastNode!["main"]!;
+        // Console.WriteLine(windNode);
+        JsonNode tempNode = mainNode!["temp"]!;
+        string temp = tempNode.ToString();
+        double finalTemp = Convert.ToDouble(temp) - 273.6;
+        Console.WriteLine("Temperature: " + finalTemp.ToString("0.00") + "° C");
         Console.WriteLine("\nDone.");
     }
 }
